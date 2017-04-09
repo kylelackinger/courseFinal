65a1248c19511d9bf1d4ada96c8af8bb4d42f500
kyle.lackinger <kylelackinger@gmail.com>
2017-04-09 07:34:20 -0400

    updated readme

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
-## Load "dplyr" library
+## Load necessary libraries
 library(dplyr)
+library(reshape2)
 
 ## Set the working directory appropriately
 setwd("~/Coursera/Data_Science/Course_Projects/3_Getting_and_Cleaining_Data/courseFinal")
@@ -17,11 +18,10 @@ testData <- cbind(testActivity, testSubject, test)
 
 training <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
 trainingActivity <- read.table("./data/UCI HAR Dataset/train/Y_train.txt")
-trainSubject <- read.table("./data/UCI HAR Dataset/train/Y_train.txt")
+trainSubject <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 trainingData <- cbind(trainingActivity, trainSubject, training)
 
 fullData <- rbind(testData, trainingData)
-fullData <- tbl_df(fullData)
 
 ## Read in the feature names that make up the dataframe variable names and apply to complete DF
 featureNames <- read.table("./data/UCI HAR Dataset/features.txt")
@@ -38,4 +38,9 @@ for(i in 1:6) {
 means <- grep("mean", names(fullData))
 stds <- grep("std", names(fullData))
 extractIndex <- sort(c(1, 2, means, stds))
-statData <- fullData[, extractIndex]
+statData <- tbl_df(fullData[, extractIndex])
+
+## Reshape data to compute mean for each subject and activity
+varNames <- names(statData)[3:ncol(statData)]
+statMelt <- melt(statData, id = c("activity", "subjectNumber"), measure.vars = varNames)
+avgData <- dcast(statMelt, activity + subjectNumber ~ variable, mean)
