ba62fbb40796b5e03ad148196010772ebfc3e7bc
kyle.lackinger <kylelackinger@gmail.com>
2017-04-09 09:05:53 -0400

    updated analysis script

--- run_analysis.R
+++ run_analysis.R
@@ -2,9 +2,6 @@
 library(dplyr)
 library(reshape2)
 
-## Set the working directory appropriately
-setwd("~/Coursera/Data_Science/Course_Projects/3_Getting_and_Cleaining_Data/courseFinal")
-
 ## Download the zipped file from the specified source and unzip to a directory
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 if(!file.exists("./data")) {dir.create("./data")}
